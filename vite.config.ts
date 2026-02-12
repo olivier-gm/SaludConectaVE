903fcd65d1293c89c16df986e78a9d66e6cbd596
olivier-gm <152037529+olivier-gm@users.noreply.github.com>
2026-02-12 14:34:25 -0400

    update2

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,11 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: parseInt(process.env.PORT || '5173'),
+        allowedHosts: true,
     },
     preview: {
         host: '0.0.0.0',
         port: parseInt(process.env.PORT || '4173'),
+        allowedHosts: true,
     },
 })
